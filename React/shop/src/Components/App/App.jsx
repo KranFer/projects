876b0090a73679427628b7e85ec257eece15d937
Pronin Vladimir <04049752vova@gmail.com>
2025-03-12 21:52:22 +0300

    Adding new files for project

---
+++ React/shop/src/Components/App/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import AppRoutes from '../Routes/Routes';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Sidebar from '../Sidebar/Sidebar';
+
+import '../../css/global.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+
+      <div className="container">
+        <Sidebar />
+        <AppRoutes/>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
